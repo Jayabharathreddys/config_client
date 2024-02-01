6af5d7ba81ef0caa09c9805a6125df008797ea16
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2024-02-01 23:03:05 +0530

    next changes

--- src/main/java/com/jb/config_client/rest/ConfigRestController.java
+++ src/main/java/com/jb/config_client/rest/ConfigRestController.java
@@ -1,2 +1,19 @@
-package com.jb.config_client;public class ConfigRestController {
+package com.jb.config_client.rest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigRestController {
+
+    @Value("${msg}")
+    private String msg;
+    @GetMapping("/")
+    public String getMsg(){
+        return  msg;
+    }
 }
